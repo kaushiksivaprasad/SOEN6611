f30eaf0227af03526b13aa1083fa6097e848a249
unknown <kaushik_sivaprasad@hotmail.com>
2015-06-04 18:28:11 -0400

    Added totNoOfMethods field

--- Code/ProjectUtils.java
+++ Code/ProjectUtils.java
@@ -17,67 +17,81 @@ public class ProjectUtils {
 	 */
 	public static HashMap<String, LinkedList<String>> inheritanceTree = new HashMap<String, LinkedList<String>>();
 	/**
-	 * This variable has details about all the packages available in the project and also classes within it.
+	 * This variable has details about all the packages available in the project
+	 * and also classes within it.
 	 */
-	public static HashMap<String, Set<String>> packageDetails = new HashMap<String, Set<String>>(); 
-    /**
-     * This variable has details about all the methods of a class`s name.
-     */
-    public static HashMap<String, List<MethodObject>> methodsOfClass = new HashMap<String, List<MethodObject>>();
-	
+	public static HashMap<String, Set<String>> packageDetails = new HashMap<String, Set<String>>();
+	/**
+	 * This variable has details about all the methods of a class`s name.
+	 */
+	public static HashMap<String, List<MethodObject>> methodsOfClass = new HashMap<String, List<MethodObject>>();
+	/**
+	 * To get the number of classes in the system.
+	 */
 	public static int totNumberOfClasses = 0;
 	
-	public static void loadProjectDetails(SystemObject obj){
+	/**
+	 * Get the total number of methods in the system..
+	 */
+	public static int totNumberOfMethods = 0;
+
+	public static void loadProjectDetails(SystemObject obj) {
 		ListIterator<ClassObject> classIterator = obj.getClassListIterator();
-		while(classIterator.hasNext()){
+		while (classIterator.hasNext()) {
 			ClassObject classObject = classIterator.next();
 			loadInheritanceDetails(classObject);
 			extractPackageLevelDetails(classObject);
 			totNumberOfClasses++;
 		}
 	}
-	
-	public static void loadInheritanceDetails(ClassObject classObject){
+
+	public static void loadInheritanceDetails(ClassObject classObject) {
 		String ancestorName = null;
 		String key = classObject.getName().trim();
-		if(classObject.getSuperclass() != null){
+		if (classObject.getSuperclass() != null) {
 			ancestorName = classObject.getSuperclass().toString();
 		}
 		LinkedList<String> children = null;
-		if(ancestorName != null && ancestorName.trim().length() > 0){ 
-			//if the current class is inherited, ancestorName will not be null
-			children = inheritanceTree.get(ancestorName); 
-			if(children == null){
+		if (ancestorName != null && ancestorName.trim().length() > 0) {
+			// if the current class is inherited, ancestorName will not be null
+			children = inheritanceTree.get(ancestorName);
+			if (children == null) {
 				children = new LinkedList<String>();
 			}
 			children.add(ancestorName);
 			key = ancestorName.trim();
 		}
 		inheritanceTree.put(key, children);
-        methodsOfClass.put(key, classObject.getMethodList());
+		methodsOfClass.put(key, classObject.getMethodList());
+		totNumberOfMethods += classObject.getMethodList().size();
 	}
-	public static void extractPackageLevelDetails(ClassObject classObject){
+
+	public static void extractPackageLevelDetails(ClassObject classObject) {
 		String name = classObject.getName().trim();
 		String packageName = extractPackageNameFromWholeClassName(name);
-		if(packageName != null){
+		if (packageName != null) {
 			Set<String> classesSet = packageDetails.get(packageName);
-			if(classesSet == null){
+			if (classesSet == null) {
 				classesSet = new HashSet<String>();
 			}
 			classesSet.add(name);
 			packageDetails.put(packageName, classesSet);
 		}
 	}
+
 	/**
-	 * This method extracts packageName out of the whole className and returns it.
+	 * This method extracts packageName out of the whole className and returns
+	 * it.
+	 * 
 	 * @param wholeClassName
 	 * @return
 	 */
-	public static String extractPackageNameFromWholeClassName(String wholeClassName){
-		if(wholeClassName != null){
+	public static String extractPackageNameFromWholeClassName(
+			String wholeClassName) {
+		if (wholeClassName != null) {
 			int indx = wholeClassName.lastIndexOf(".");
-			if(indx < wholeClassName.length() - 1){
-				return wholeClassName.substring(0,indx);
+			if (indx < wholeClassName.length() - 1) {
+				return wholeClassName.substring(0, indx);
 			}
 		}
 		return null;
